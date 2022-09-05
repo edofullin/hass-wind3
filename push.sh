a53bfd1291e74d8b0a90db12ede116ea810ad67a
Edoardo Fullin <edoardo.fullin@outlook.com>
2022-09-05 10:52:33 +0000

    added hacs.json

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /workspaces/core/config/custom_components
+rm -r /workspaces/core/config/custom_components
+cp -r /workspaces/dev_wind3/custom_components/wind3 /workspaces/core/config/custom_components
